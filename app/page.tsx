6eb8e0d29400d1a7a980e45557bbbe01703a03e8
david gomes <davidgomesk8@gmail.com>
2025-08-21 11:22:09 +0200

    added button and image to shop test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   const products = await stripe.products.list({
@@ -16,7 +17,16 @@ export default async function Home() {
           <div>
             <h2>Products of Paul Cardim</h2>
             <p>Discover my products all handmade</p>
+            <Button asChild variant={"default"}>
+              <Link href="/shop">Shop</Link>
+            </Button>
           </div>
+          <Image
+            alt="Product Image"
+            src={products.data[0].images[0]}
+            width={500}
+            height={500}
+          />
         </div>
       </section>
     </div>
